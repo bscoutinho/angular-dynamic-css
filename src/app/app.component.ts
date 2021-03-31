a4da2ee8cbd6d950baf73a33d4331145b99ec325
DESKTOP-HVU60H0\Bruno Coutinho <bscoutinho2012@gmail.com>
2021-03-31 23:18:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dynamic-css';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+  }
+
+  loadTheme(cssFile: string) {
+
+    const headEl = this.document.getElementsByTagName('head')[0];
+
+    const existingLinkEL = this.document.getElementById('client-theme') as HTMLLinkElement;
+
+    if (existingLinkEL) {
+      existingLinkEL.href = cssFile;
+    } else {
+      const newLinkEl = this.document.createElement('link');
+      newLinkEl.rel = 'stylesheet';
+      newLinkEl.href = cssFile; 
+      headEl.appendChild(newLinkEl);
+    }
+  }
 }
